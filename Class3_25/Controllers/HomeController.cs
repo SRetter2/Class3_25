608ed5ef2a061a1ca2064906675d6e897a899467
Sarala Retter <saralaretter@gmail.com>
2020-04-01 21:30:26 -0400

    Add project files.

---
+++ Class3_25/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Class3_25.Models;
+
+namespace Class3_25.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            string value = Request.Cookies["number"];
+            bool first = String.IsNullOrEmpty(value);
+            int num = int.Parse(value);
+            if (!first)
+            {
+                num = 1;
+                Response.Cookies.Append("number", $"{num}");               
+               
+            }
+            else
+            {
+                num++;
+            }
+            return View(num);
+        }
+
+        
+    }
+}
---
+++ Class3_25/Models/CookieViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class3_25.Models
+{
+    public class CookieViewModel
+    {
+        public int Number { get; set; }
+        public bool FirstTime { get; set; }
+    }
+}
